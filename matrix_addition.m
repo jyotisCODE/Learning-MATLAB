eeadd0c72866c192dfbccb6296449d161fadb90a
jyotisCODE <jyotisagwan19@gmail.com>
2025-08-05 22:32:51 +0530

    Add files via upload

---
+++ matrix_addition.m
@@ -0,0 +1,15 @@
+% Script for Matrix Addition 
+
+A = [1 2 3; 4 5 6; 7 8 9];
+B = [1 1 1; 2 2 2; 3 3 3];
+
+C = A + B;
+
+disp('Matrix A: ');
+disp(A);
+
+disp('Matrix B: ');
+disp(B);
+
+disp('Addition of A and B: ');
+disp(C)
