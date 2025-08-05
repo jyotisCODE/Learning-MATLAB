41333be18ed19d62d8206d8e8c5468bfa331f34c
jyotisCODE <jyotisagwan19@gmail.com>
2025-08-05 22:35:43 +0530

    Add files via upload

---
+++ diag_of_sq_matrix.m
@@ -0,0 +1,11 @@
+% Script to extract and display the main diagonal of a square matrix
+
+A = [1 1 1; 2 2 2; 3 3 3];
+
+diagonal = diag(A);
+
+disp('Matrix A: ');
+disp(A);
+
+disp('Main diagonal of A: ');
+disp(diagonal)
