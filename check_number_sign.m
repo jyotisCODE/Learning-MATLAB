ae7c478c98b7dee3a558bce5dbed24882bc12efa
jyotisCODE <jyotisagwan19@gmail.com>
2025-08-10 23:35:09 +0530

    Add files via upload

---
+++ check_number_sign.m
@@ -0,0 +1,12 @@
+% Script to check whether a number is positive, negative or zero
+
+number = input('Enter a number: ');
+
+if number > 0
+    disp('Positive');
+elseif number < 0 
+    disp('Negative');
+else 
+    disp('Zero');
+end    
+ 
