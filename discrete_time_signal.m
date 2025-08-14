714d1959760b8dd0b06e8df6d4ac9bc3db9dc66b
jyotisCODE <jyotisagwan19@gmail.com>
2025-08-14 22:01:25 +0530

    Delete discrete_time_signal.m

--- discrete_time_signal.m
+++
@@ -1,8 +0,0 @@
-% Discrete-time sinusoid
-n = 0:20;            % Time index
-x = cos(0.2*pi*n);   % Signal
-
-stem(n, x, 'filled');
-title('Discrete-Time Cosine Signal');
-xlabel('n'); ylabel('x[n]');
-grid on;
